4db1b7d1bdfbd0bb0401b371016302d5470e8656
ag7621 <aaron.c.guerra@gmail.com>
2025-01-04 18:54:32 -0700

    update main.jsx with fetch
    added basic fetch request for pokeapi

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const url = "https://pokeapi.co/api/v2/pokemon?limit=10"
 
 function Main() {
+    const [pokeData, setPokeData] = useState([]);
+
+    const getPokeData = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log('fetched data: ', data.results);
+    }
+
+    useEffect(() => {
+        getPokeData();
+    }, []);
+
     return (
         <div>Main</div>
     )
